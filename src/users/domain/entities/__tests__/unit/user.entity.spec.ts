14ce3d30e13d568b8b7a61578d832a822ade3cb3
Claudio Henrique <simonelliclaudiohenrique@gmail.com>
2023-11-06 11:03:07 -0400

    feat: add user data builder helper method

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,16 +1,12 @@
-import { faker } from "@faker-js/faker";
 import { UserEntity, UserProps } from "../../user.entity";
+import { UserDataBuilder } from "../../../../domain/testing/helpers/user-data-builder";
 
 describe("UserEnttity unit tests", () => {
   let props: UserProps;
   let sut: UserEntity;
 
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
+    props = UserDataBuilder({});
 
     sut = new UserEntity(props);
   });
