4b7dcfb67553bb537ced652d21cc5bba738153f0
Claudio Henrique <simonelliclaudiohenrique@gmail.com>
2023-11-17 17:45:47 -0400

    refactor: includs find all method

--- src/shared/domain/repositories/repository-contracts.ts
+++ src/shared/domain/repositories/repository-contracts.ts
@@ -2,7 +2,7 @@ import { UserEntity } from "@/users/domain/entities/user.entity";
 
 export interface RepositoryInterface<E extends UserEntity> {
   insert(entity: E): Promise<void>;
-  findById(id: number): Promise<number>;
+  findById(id: number): Promise<E>;
   findAll(): Promise<E[]>;
   update(entity: E): Promise<void>;
   delete(id: number): Promise<void>;
