39687a689f526c68d40a9d371d68b2e2b850b637
Claudio Henrique <simonelliclaudiohenrique@gmail.com>
2023-12-25 18:04:26 -0400

    test: add search params tests

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -4,7 +4,7 @@ import { RepositoryInterface } from "./repository-contracts";
 export type SearchDirection = "asc" | "desc";
 export type SearchProps<Filter = string> = {
   page?: number;
-  perPage?: 15;
+  perPage?: number;
   sort?: string | null;
   sortDir?: SearchDirection | null;
   filter?: Filter | null;
@@ -12,17 +12,17 @@ export type SearchProps<Filter = string> = {
 
 export class SearchParams {
   protected _page: number;
-  protected _perPage: number;
+  protected _perPage = 15;
   protected _sort: string;
   protected _sortDir: string;
   protected _filter: string;
 
-  constructor(props: SearchProps) {
-    this._page = props.page;
-    this._perPage = props.perPage;
-    this._sort = props.sort;
-    this._sortDir = props.sortDir;
-    this._filter = props.filter;
+  constructor(props: SearchProps = {}) {
+    this.page = props.page;
+    this.perPage = props.perPage;
+    this.sort = props.sort;
+    this.sortDir = props.sortDir;
+    this.filter = props.filter;
   }
 
   get page() {
@@ -30,15 +30,38 @@ export class SearchParams {
   }
 
   private set page(value: number) {
-    this._page = value;
+    let _page = +value;
+    if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) !== _page) {
+      _page = 1;
+    }
+
+    this._page = _page;
   }
 
-  get sor() {
+  get perPage() {
+    return this._perPage;
+  }
+
+  private set perPage(value: number) {
+    let _perPage = value === (true as any) ? this._perPage : +value;
+    if (
+      Number.isNaN(_perPage) ||
+      _perPage <= 0 ||
+      parseInt(_perPage as any) !== _perPage
+    ) {
+      _perPage = this._perPage;
+    }
+
+    this._perPage = _perPage;
+  }
+
+  get sort() {
     return this._sort;
   }
 
   private set sort(value: string | null) {
-    this._sort = value;
+    this._sort =
+      value === null || value === undefined || value === "" ? null : `${value}`;
   }
 
   get sortDir() {
@@ -46,7 +69,13 @@ export class SearchParams {
   }
 
   private set sortDir(value: string | null) {
-    this._sortDir = value;
+    if (!this.sort) {
+      this._sortDir = null;
+      return;
+    }
+
+    const dir = `${value}`.toLocaleLowerCase();
+    this._sortDir = dir !== "asc" && dir !== "desc" ? "desc" : dir;
   }
 
   get filter() {
@@ -54,7 +83,8 @@ export class SearchParams {
   }
 
   private set filter(value: string | null) {
-    this._filter = value;
+    this._filter =
+      value === null || value === undefined || value === "" ? null : `${value}`;
   }
 }
 
